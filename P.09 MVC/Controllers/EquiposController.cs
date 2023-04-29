d5a963fbd1165b542bde7decca1cd14975f05c08
LuisGo1 <luis.gomez1@catolica.edu.sv>
2023-04-29 01:11:00 -0600

    Agregar archivos de proyecto.

---
+++ P.09 MVC/Controllers/EquiposController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using P._09_MVC.Models;
+using PracticaMVC.Models;
+
+namespace PracticaMVC.Controllers
+{
+    public class EquiposController : Controller
+    {
+        private readonly equiposDbContext _equiposDbContext;
+
+        public EquiposController(equiposDbContext equiposDbContext)
+        {
+            _equiposDbContext = equiposDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var listadeMarcas = (from m in _equiposDbContext.marcas select m).ToList();
+            ViewData["listadodeMarcas"] = new SelectList(listadeMarcas, "id_marcas", "nombre_marca");
+
+            var listadodeEquipos = (from e in _equiposDbContext.equipos
+                                    join m in _equiposDbContext.marcas on e.marca_id equals m.id_marcas
+                                    select new
+                                    {
+                                        nombre = e.nombre,
+                                        descripcion = e.descripcion,
+                                        marca_id = e.marca_id,
+                                        marca_nombre = m.nombre_marca
+                                    }).ToList();
+
+            ViewData["listadoEquipo"] = listadodeEquipos;
+
+            return View();
+        }
+
+        public IActionResult CrearEquipos(equipos nuevoEquipo)
+        {
+            _equiposDbContext.Add(nuevoEquipo);
+            _equiposDbContext.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+
+
+}
---
+++ P.09 MVC/Models/equiposDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.Identity.Client;
+using P._09_MVC.Models;
+
+namespace PracticaMVC.Models
+{
+    public class equiposDbContext : DbContext
+    {
+        public equiposDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<equipos> equipos { get; set; }
+        public DbSet<marcas> marcas { get; set; }
+      
+    }
+
+}
